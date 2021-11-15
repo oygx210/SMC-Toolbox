5e5b9720fa2e81e168696e7f9a3964637f0821c0
w.schwamm <w.schwamm@CNB2443.corp.grimme.com>
2021-11-15 07:37:10 +0100

    worked on pendulum example

--- mscripts/get_legend.m
+++ mscripts/get_legend.m
@@ -1,16 +1,28 @@
-function [leg] = get_legend(data_str)
+function [leg] = get_legend(data_str,indices)
 %GET_LEGEND Summary of this function goes here
 %   Construct the plot legend for an data array by adding the
 %   incremental indices
 
 data=evalin('base',data_str);
+
 if strcmp(class(data),'timeseries')
     sz=size(data.Data,2);
 else
     sz=size(data,2);
 end
+
+if nargin>1
+    if max(indices)>sz
+        error(['max index ',num2str(max(indices)),' is greater than size ',num2str(sz)]);
+    else
+        idx=indices;
+    end
+else
+    idx=1:sz;
+end
+
 if sz>1
-    leg=strcat(data_str,string(num2cell(1:sz)));
+    leg=strcat(data_str,string(num2cell(idx)));
 else
     leg={data_str};
 end
--- simulink/pendulum_data.m
+++ simulink/pendulum_data.m
@@ -1,5 +1,4 @@
-%% Chap. 1.5: Double Integrator - State-space Approach
-%% Chap. 6.4: Robust Observer Design
+%% Chap. 6.4: Robust Observer Design for pendulum
 
 clear all;
 close all;
@@ -16,31 +15,31 @@ C=[1 1];
 psm = [-1 -1];
 per = -1;
 
-rho=0.1;
 [Gl,Gn,P2]=disobs(A,B,C,psm,per);
-Tc=[1 0; 1 1];
-Ac=Tc*A*inv(Tc);
-Bc=Tc*B;
-Cc=C*inv(Tc);
+% Tc=[1 0; 1 1];
+% Ac=Tc*A*inv(Tc);
+% Bc=Tc*B;
+% Cc=C*inv(Tc);
 
 % alternative calculation of canonical form
 [Ac,Bc,Cc,Tc] = obsfor(A,B,C);
 
 
-% recompute the Lyapunov matrix with modifid weighting
-Q2=2;
-a22s=diag(per,0);
-P2=lyap(a22s',diag(Q2));
-
-
+% recompute the Lyapunov matrix with modified weighting
+% Q2=2;
+% a22s=diag(per,0);
+% P2=lyap(a22s',diag(Q2));
 
 x0=zeros(1,n);
 x0(1)=1.5;
 
+z0=zeros(1,n);
+
+% design parameter for the switching function
 rho=1.0;
 delta=0.01;
 
-SimStopTime=20;
+SimStopTime=10;
 
 %% simulate the model
 mdl_name='pendulum_mdl';
@@ -51,33 +50,34 @@ set_param(mdl_name,'StopTime',sprintf('%d',SimStopTime));
 sim(mdl_name);
 
 %% plot simulation results
-subplot(2,2,1)
-plot(t.Data,s.Data);
+subplot(2,3,1)
+plot(t.Data,u.Data);
 grid on;
-legend(get_legend('s'));
+legend(get_legend('u'));
 
-xc=xhat.Data(:,2);
-x11=x.Data(:,2);
-x12=x.Data(:,3);
-ec=xc-x11-Lo*x12;
-
-xr=x.Data(:,1);
-zr=xhat.Data(:,1);
-er=zr-xr;
-
-subplot(2,2,2)
-plot(t.Data,ec,t.Data,er);
+subplot(2,3,2)
+plot(t.Data,ul.Data,t.Data,un.Data);
 grid on;
-legend([get_legend('ec');get_legend('er')]);
+legend([get_legend('ul');get_legend('un')]);
 
-subplot(2,2,3)
-plot(t.Data,x.Data);
+subplot(2,3,3)
+plot(t.Data,ey.Data);
 grid on;
-legend(get_legend('x'));
+legend(get_legend('ey'));
 
-subplot(2,2,4)
-plot(t.Data,xhat.Data);
+subplot(2,3,4)
+plot(t.Data,y.Data,t.Data,yo.Data);
 grid on;
-legend(get_legend('xhat'));
+legend([get_legend('y');get_legend('yo')]);
 
-sgtitle([mdl_name,' - ',mat_name],'Interpreter','None');
+subplot(2,3,5)
+plot(t.Data,x.Data(:,1),t.Data,z.Data(:,1));
+grid on;
+legend([get_legend('x',1);get_legend('z',1)]);
+
+subplot(2,3,6)
+plot(t.Data,x.Data(:,2),t.Data,z.Data(:,2));
+grid on;
+legend([get_legend('x',2);get_legend('z',2)]);
+
+sgtitle(mdl_name,'Interpreter','None');
